9cde8e3b536dfc4d40412f656023738441f4abe5
walter_johnson <walter_johnson@trimble.com>
2019-02-06 16:58:59 -0800

    worked on test.js

--- test.js
+++ test.js
@@ -4,21 +4,28 @@
 
 //one global variable to rule them all
 var DEG = {
-    cells: document.getElementsByClassName("cell"),
+    cells: Array.from(document.getElementsByClassName("cell")),
     focusCell: null,
     upButton: document.getElementById("up"),
     activeElm: 0,
+    focusedCell: 0,
 };
 
 DEG.focusCell = function (cellIndex) {
     DEG.cells[cellIndex].focus();
+    DEG.focusedCell = cellIndex;
 };
 
 DEG.focusCell(0);
 
 DEG.upButton.addEventListener("click", function () {
-    DEG.focusCell(3);
-    DEG.activeElm = document.activeElement;
+    var current = DEG.cells.indexOf(document.activeElement);
+    console.log("current: " + current);
+    if(current > 4) {
+        DEG.focusCell(current - 4);
+    }
+    // DEG.focusCell(3);
+    // DEG.activeElm = document.activeElement;
 });
 
-console.log(DEG.activeElm);
+// console.log(DEG.activeElm);
