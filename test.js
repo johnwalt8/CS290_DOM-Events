0683dda4f002f7de2c7f2e12282039a01f766588
johnwalt8 <46585029+johnwalt8@users.noreply.github.com>
2019-02-06 20:45:14 -0800

    test fully functional

--- test.js
+++ test.js
@@ -5,27 +5,70 @@
 //one global variable to rule them all
 var DEG = {
     cells: Array.from(document.getElementsByClassName("cell")),
+    body: document.getElementsByTagName("body"),
+    tbody: document.getElementsByTagName("tbody"),
     focusCell: null,
     upButton: document.getElementById("up"),
+    downButton: document.getElementById("down"),
+    leftButton: document.getElementById("left"),
+    rightButton: document.getElementById("right"),
+    markButton: document.getElementById("mark"),
     activeElm: 0,
     focusedCell: 0,
 };
 
+DEG.body[0].addEventListener("focusin", function() {
+    var current = DEG.cells.indexOf(document.activeElement);
+    if (current > -1) {
+        DEG.focusedCell = current;
+    }
+});
+
 DEG.focusCell = function (cellIndex) {
     DEG.cells[cellIndex].focus();
-    DEG.focusedCell = cellIndex;
 };
 
 DEG.focusCell(0);
 
-DEG.upButton.addEventListener("click", function () {
-    var current = DEG.cells.indexOf(document.activeElement);
-    console.log("current: " + current);
-    if(current > 4) {
-        DEG.focusCell(current - 4);
+DEG.body[0].addEventListener("focus", function() {
+    if (document.activeElement === DEG.body[0]) {
+        DEG.focusCell(DEG.focusedCell);
     }
-    // DEG.focusCell(3);
-    // DEG.activeElm = document.activeElement;
 });
 
-// console.log(DEG.activeElm);
+DEG.upButton.addEventListener("click", function () {
+    if(DEG.focusedCell > 4) {
+        DEG.focusCell(DEG.focusedCell - 4);
+    } else {
+        DEG.focusCell(DEG.focusedCell);
+    }
+});
+
+DEG.downButton.addEventListener("click", function() {
+    if(DEG.focusedCell < 8) {
+        DEG.focusCell(DEG.focusedCell + 4);
+    } else {
+        DEG.focusCell(DEG.focusedCell);
+    }
+});
+
+DEG.rightButton.addEventListener("click", function () {
+    if(DEG.focusedCell !== 3 && DEG.focusedCell !== 7 && DEG.focusedCell !== 11) {
+        DEG.focusCell(DEG.focusedCell + 1);
+    } else {
+        DEG.focusCell(DEG.focusedCell);
+    }
+});
+
+DEG.leftButton.addEventListener("click", function () {
+    if(DEG.focusedCell !== 0 && DEG.focusedCell !== 4 && DEG.focusedCell !== 8) {
+        DEG.focusCell(DEG.focusedCell - 1);
+    } else {
+        DEG.focusCell(DEG.focusedCell);
+    }
+});
+
+DEG.markButton.addEventListener("click", function () {
+    DEG.cells[DEG.focusedCell].style.backgroundColor = "yellow";
+    DEG.focusCell(DEG.focusedCell);
+});
